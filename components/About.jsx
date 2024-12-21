f5410493cf642331afe28cb7044c1f450e89bf48
Francisco Tulcan DevFJTR <team@aadaggio.com.co>
2024-12-20 22:15:36 -0500

    inicio section about

--- components/About.jsx
+++ components/About.jsx
@@ -113,11 +113,34 @@ const About = () => {
     return arr.find((item) => item.title === title)  
   };
 
-  console.log(getData(quialificationData, 'education'))
-
-  return  <div>
-      About
-    </div>
+  return  <section className="xl:h-[860px] pb-12 xl:py-24">
+      <div className="container mx-auto">
+        <h2 className="mx-auto text-center section-title mb_8 xl:mb-16">About me</h2>
+        <div className="flex flex-col xl:flex-row">
+          {/* {image} */}
+          <div className="relative flex-1 hidden xl:flex">
+            <DevImg containerStyles='bg-about_shape_light dark:bg-about_shape_dark w-[505px] h-[505px] bg-no-repeat relative' imgSrc='/about/developer.png' />
+          </div>
+          {/* tabs */}
+          <div className="flex-1 p-2 rounded-none">
+            <Tabs defaultValue="personal">
+              <TabsList>
+                <TabsTrigger value="personal">Personal Info</TabsTrigger>
+                <TabsTrigger value="qualification">Qualification</TabsTrigger>
+                <TabsTrigger value="skills">Skills</TabsTrigger>
+              </TabsList>
+              {/* Tabs content */}
+              <div className="mt-12 text-lg xl:mt-8">
+                {/* Personal */}
+                <TabsContent value="personal">personal info</TabsContent>
+                <TabsContent value="qualification">quialification info</TabsContent>
+                <TabsContent value="skills">skills info</TabsContent>
+              </div>
+            </Tabs>
+          </div>
+        </div>
+      </div>
+    </section>
 
 }
 
